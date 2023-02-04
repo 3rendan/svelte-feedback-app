f4294c7cde319fdaef4456ed783476d8b538925a
Brendan Ryan <brendan.ryan@pm.me>
2023-02-04 14:19:30 -0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte'
+	let feedback = [
+		{
+			id: 1,
+			rating: 8,
+			text: 'First of all, welcome to Linux! I, too, transferred from Mac to Linux years ago, and I think it is a great way to transition.'
+		},
+		{
+			id: 2,
+			rating: 10,
+			text: 'First of all, welcome to Linux! I, too, transferred from Mac to Linux years ago, and I think it is a great way to transition.'
+		},
+		{
+			id: 3,
+			rating: 6,
+			text: 'First of all, welcome to Linux! I, too, transferred from Mac to Linux years ago, and I think it is a great way to transition.'
+		}
+	]
+	const deleteFeedback = (e) => { 
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id != itemId)
+	 }
+</script>
+
+<main class='container'>
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
+
+<style>
+	
+</style>
